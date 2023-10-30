1ff7a3fb2d5dc91143d282b951b11463d11bf546
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-10-30 20:52:42 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-
+/*
 //Preparando para produccion
 if(process.env.NODE_ENV === 'production'){
   //Seteando la carpeta estatica
@@ -71,15 +71,15 @@ if(process.env.NODE_ENV === 'production'){
       `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
     )
   );
-}
+}*/
 
 
 
 
-/*
+
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-);*/
+);
