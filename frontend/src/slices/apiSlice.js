0f7a183750f290b2c64f82893ff24792c5ca8670
gonzacebre <gonzacebre18@gmail.com>
2023-10-30 21:55:57 -0300

    ultima prueba

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,9 +1,9 @@
 //Este archivo va a llamar a las otras slices
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BASE_URL } from "../constants";
+import { BASE_URL2 } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL2 });
 
 export const apiSlice = createApi({
   baseQuery, //Recibe la url base del sitio
