fddda1b33cd16f69dd757572fa49d20fa8bec8d9
gonzacebre <gonzacebre18@gmail.com>
2023-10-30 21:27:27 -0300

    hola 2

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,21 +1,9 @@
 //Este archivo va a llamar a las otras slices
 
-/* import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BASE_URL } from "../constants";
-
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
-
-export const apiSlice = createApi({
-  baseQuery, //Recibe la url base del sitio
-  tagTypes: ["Product", "Order", "User"], //son los diferentes tipos de data que va a importar
-  endpoints: (builder) => ({}),
-});
- */
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://staychill-ecommerce-api.vercel.app/' });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
   baseQuery, //Recibe la url base del sitio
