bb65b593ebe92b8c50c88635707d2b72ecea0a78
gonzacebre <gonzacebre18@gmail.com>
2023-10-31 20:01:06 -0300

    solucionando problema de jwt

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,10 @@ const generateToken = (res, userId) => {
   // Set JWT as HTTP-only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    secure: true,
+    /* secure: process.env.NODE_ENV !== "development", */
+    sameSite: "none",
+    /* sameSite: "strict", */
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dias
   });
 };
