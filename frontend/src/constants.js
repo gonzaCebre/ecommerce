59c0d58bbed0822b163ce5209fef0dd2cddc6a07
gonzacebre <gonzacebre18@gmail.com>
2023-10-30 21:33:11 -0300

    hola 3

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,11 +1,11 @@
 /* export const BASE_URL =
   process.env.NODE_ENV === "development" ? "http://localhost:5000" : ""; */
-export const BASE_URL = "https://staychill-ecommerce-api.vercel.app/";
-export const PRODUCTS_URL = "https://staychill-ecommerce-api.vercel.app/api/products";
-export const USERS_URL = "https://staychill-ecommerce-api.vercel.app/api/users";
-export const ORDERS_URL = "https://staychill-ecommerce-api.vercel.app/api/orders";
-export const PAYPAL_URL = "https://staychill-ecommerce-api.vercel.app/api/config/paypal";
-export const MERCADOPAGO_URL_PUBLIC_KEY = "https://staychill-ecommerce-api.vercel.app/api/config/mercadopago";
-export const MERCADOPAGO_URL = "https://staychill-ecommerce-api.vercel.app/api/payment";
-export const UPLOADS_URL = "https://staychill-ecommerce-api.vercel.app/api/upload";
-export const INSTAGRAM_URL = "https://staychill-ecommerce-api.vercel.app/api/config/instagram";
+export const BASE_URL = "https://staychill-ecommerce-api.vercel.app";
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
+export const MERCADOPAGO_URL_PUBLIC_KEY = "/api/config/mercadopago";
+export const MERCADOPAGO_URL = "/api/payment";
+export const UPLOADS_URL = "/api/upload";
+export const INSTAGRAM_URL = "/api/config/instagram";
