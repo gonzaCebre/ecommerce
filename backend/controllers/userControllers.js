5946217f8ec1c2a36481b093cafe8a71a4df9581
gonzacebre <gonzacebre18@gmail.com>
2023-11-04 18:17:07 -0300

    seteando token para produccion

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -22,10 +22,19 @@ const authUser = asyncHandler(async (req, res) => {
     // Set JWT as HTTP-only cookie
     res.cookie("jwt", token, {
       httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dias
+      domain: "https://frontend-delta-rouge-29.vercel.app/"
+    });
+
+/*     // Para desarrollo
+    res.cookie("jwt", token, {
+      httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
       sameSite: "strict",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dias
-    });
+    }); */
 
     /* generateToken(res, user._id);  */
 
