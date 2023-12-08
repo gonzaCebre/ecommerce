002f5bfe6b7ab0b1507700153cb523fdf974be3d
gonzacebre <gonzacebre18@gmail.com>
2023-12-08 16:40:07 -0300

    CORS solucionado

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(cookieParser()); //Nos permite acceder a las cookies
 //Para produccion
 const corsOptions = {
   origin: "https://frontend-delta-rouge-29.vercel.app", // Reemplaza con el origen exacto de tu cliente
-  credentials: true, // Habilita las solicitudes con credenciales
+  /* credentials: true, */ // Habilita las solicitudes con credenciales
 };
 
 
