7c65fe0ecc0aebe0a627ff47db1090f8b2e21cce
gonzacebre <gonzacebre18@gmail.com>
2023-12-08 17:34:16 -0300

    arreglos

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -85,13 +85,13 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: (data) => {
         const token = localStorage.getItem('token');
 
-        const formData = new FormData();
-        formData.append('image', data); // Asegúrate de que 'data' contenga la imagen
+        /* const formData = new FormData();
+        formData.append('image', data); // Asegúrate de que 'data' contenga la imagen */
 
         return{
           url: `${UPLOADS_URL}`,
           method: 'POST',          
-          body: formData,
+          body: data,
           headers: {
             'Authorization': `Bearer ${token}`,
           },
