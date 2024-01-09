a80ed7d6c93c5954a22b22658301af6428452b2e
gonzacebre <gonzacebre18@gmail.com>
2024-01-09 19:36:51 -0300

    opengraph

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,10 +1,14 @@
 import Categories from "../components/Categories";
 import Advertise from "../components/Advertise";
 import FeedInstagram from "../components/FeedInstagram";
+import { Helmet } from "react-helmet-async";
 
 const HomeScreen = () => {
   return (
     <>
+      <Helmet>
+        <meta property="og:title" content="Título Open Graph" />
+      </Helmet>
       <div className="foto-index__container">
         <img src={require("../media/foto-index.jpg")} alt="" />
       </div>
