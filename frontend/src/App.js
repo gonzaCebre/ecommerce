e0338e8032b2c47fdb210d3cf575a4fefd6898d1
gonzacebre <gonzacebre18@gmail.com>
2024-01-09 20:35:13 -0300

    agrego etiquetas opengraph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,20 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { Helmet } from "react-helmet-async";
 
 const App = () => {
 
   return (
     <>
+      <Helmet>
+            <title>Stay Chill</title>
+            <meta property="og:title" content="Stay Chill" />
+            <meta property="og:description" content="Cultura cannabica" />
+            <meta property="og:image" content="https://res.cloudinary.com/dpaxljhpr/image/upload/v1704843127/portada_elw0go.png" />
+            <meta property="og:url" content="https://frontend-delta-rouge-29.vercel.app/" />
+            <meta property="og:type" content="website" />
+      </Helmet>
       <Header />
       <main>
           <Outlet />
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,14 +1,10 @@
 import Categories from "../components/Categories";
 import Advertise from "../components/Advertise";
 import FeedInstagram from "../components/FeedInstagram";
-import { Helmet } from "react-helmet-async";
 
 const HomeScreen = () => {
   return (
     <>
-      <Helmet>
-        <meta property="og:title" content="Título Open Graph" />
-      </Helmet>
       <div className="foto-index__container">
         <img src={require("../media/foto-index.jpg")} alt="" />
       </div>
